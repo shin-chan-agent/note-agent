6600a603f546924a8f11b9d24345bbf896e793c0
shin-chan-agent <shinya.k.828@gmail.com>
2026-08-17 20:59:07 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ from utils.latest_info import fetch_latest_info
 from utils.line_sender import (
     send_line_messages,
     create_text_message,
+    create_image_message,
 )
 from utils.logger import (
     log_info,
